d2d14d38cc853cdba81c3e5833ea591125ac001c
Robert Buessow <buessow@gmail.com>
2023-11-27 13:33:54 +0100

    add plot

--- lib/LogNormActionTest.py
+++ lib/LogNormActionTest.py
@@ -1,7 +1,11 @@
 import unittest
 import LogNormAction as lna
+import datetime as datetime
 
 class LogNormActionAction(unittest.TestCase):
 
   def test_create(self):
     lna.LogNormAction(3)
+
+  def test_plot(self):
+    lna.LogNormAction(3).plot(datetime.timedelta(hours(1))
